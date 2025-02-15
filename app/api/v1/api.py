e98f27cf1e01ae545311c3333fb52e16c0dc59b4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-15 21:20:09 +0000

    Updating to latest FlutterFlow output.

--- app/api/v1/api.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from app.api.v1.endpoints.users import users_bp
-from app.api.v1.endpoints.auth import auth_bp
-
-api_bp = Blueprint("api", __name__)
-
-# Register endpoint blueprints
-api_bp.register_blueprint(auth_bp)
-api_bp.register_blueprint(users_bp, url_prefix="/users") 
